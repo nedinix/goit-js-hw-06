41493d9b42cfedbb62aae754d52355483d140f6c
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-03-22 23:23:54 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const categories = document.querySelector("#categories");
 console.log("Number of categories: ", [...categories.children].length);
 
 const listCategories = [...categories.children].forEach((item) => {
-	console.log("Category: ", item.querySelector("h2").textContent);
-	console.log("Elements: ", item.querySelectorAll("li").length);
+	console.log("Category: ", item.firstElementChild.textContent);
+	console.log("Elements: ", item.lastElementChild.children.length);
 });
