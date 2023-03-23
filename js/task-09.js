d87af31b57e8ed0995f6a4aeb3c1c07dd25ffdc1
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-03-23 20:08:36 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+const bodyEl = document.querySelector("body");
+const textColorEl = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onMouseClick);
+
+function onMouseClick() {
+	const color = getRandomHexColor();
+	bodyEl.style.backgroundColor = color;
+	textColorEl.textContent = color;
 }
